fb6108b0e1e184a4db11c36c9b0455eb3f32eabe
TOXIR_DEV <TOXA-DEV-R>
2022-12-25 11:52:20 +0600

    codes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+require('dotenv');
+
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
 app.listen(process.env.PORT || 3000)
